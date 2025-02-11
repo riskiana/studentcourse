05ac1f17f824db6343a1bb192ad19bd754e341c5
Riskiana Wulan <riskiana.wulan@kuelap.io>
2025-02-11 10:02:34 +0700

    add activity logic

--- app/src/main/java/com/ubl/studentcourse/MainActivity.java
+++ app/src/main/java/com/ubl/studentcourse/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ubl.studentcourse;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nimText, nameText;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //findViews
+        nimText = findViewById(R.id.et_nim);
+        nameText = findViewById(R.id.et_name);
+        btnSave = findViewById(R.id.btn_save);
+
+        handleSaveButton();
+
+    }
+
+    private void handleSaveButton() {
+        btnSave.setOnClickListener(view -> {
+            String nim = nimText.getText().toString();
+            String name = nameText.getText().toString();
+            Toast.makeText(MainActivity.this, nim + " created successfully!", Toast.LENGTH_SHORT).show();
+        });
 
     }
 }
